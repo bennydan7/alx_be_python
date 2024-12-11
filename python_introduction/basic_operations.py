09f3f82f2226ea771289eac0d310571227230914
bennydan7 <owusub243@gmail.com>
2024-12-11 19:03:40 +0000

    python file for basic arithmetic

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+number1 = 10
+number2 = 5
+
+# Addition
+result1 = number1 + number2
+print(f"Addition of {number1} and {number2} is {result1}")
+
+# Subtraction
+result2 = number1 - number2
+print(f"Subtraction of {number1} and {number2} is {result2}")
+
+# Multiplication
+result3 = number1 * number2
+print(f"Multiplication of {number1} and {number2} is {result3}")
